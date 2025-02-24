629130daf81d39d3494e753f0b9310bf6c3f5c1f
geriiy <gheriyaj@gmail.com>
2025-02-24 21:46:16 +0700

    update kategori

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -8,12 +8,15 @@ use Illuminate\Support\Facades\DB;
 class KategoriController extends Controller
 {
     public function index(){
-        $data = [
-            'kategori' => 'SNK',
-            'kategori_nama' => 'Snack/Makanan Ringan',
-            'created_at' => now()
-        ];
-        DB::table('m_kategori')->insert($data);
-        return 'Insert data baru berhasil';
+        // $data = [
+        //     'kategori_kode' => 'SNK',
+        //     'kategori_nama' => 'Snack/Makanan Ringan',
+        //     'created_at' => now()
+        // ];
+        // $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->update(['kategori_nama' => 'Camilan']);
+        // return 'Update data berhasil. Jumlah data yang diupdate: ' . $row.' baris';
+
+        $data = DB::table('m_kategori')->get();
+        return view('kategori', ['data' => $data]);
     }
 }
