42b3a41636a0c146cd42508e1279d3d540d5222c
geriiy <gheriyaj@gmail.com>
2025-02-24 11:33:46 +0700

    migrate m_barang

---
+++ database/migrations/2025_02_24_035436_create_m_barang_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('m_barang', function (Blueprint $table) {
+            $table->id('barang_id'); // Auto Increment Primary Key
+            $table->string('barang_kode', 10)->unique();
+            $table->string('barang_nama', 100);
+            $table->integer('harga_beli');
+            $table->integer('harga_jual');
+            $table->integer('id_kategori');
+
+            // Foreign Key
+            $table->foreign('id_kategori')->references('kategori_id')->on('m_kategori')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('m_barang');
+        
+
+    }
+};
