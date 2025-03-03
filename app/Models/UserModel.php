5274f4cac04679e841f138e8cc76283957f0d3ac
geriiy <gheriyaj@gmail.com>
2025-03-03 09:38:38 +0700

    update user

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -10,7 +10,7 @@ class UserModel extends Model
     use HasFactory;
 
     protected $table = 'm_user';
-    protected $primaryKey = 'id_user';
+    protected $primaryKey = 'user_id';
     protected $fillable = [
         'level_id', 'username', 'nama', 'password'
     ];
