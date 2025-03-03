6bdf854fc67a6ac481842e49e6af2ae1b0350846
geriiy <gheriyaj@gmail.com>
2025-03-03 10:19:02 +0700

    save new data

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,15 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Models\UserModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index()
     {
       
-        $jumlah_data = UserModel::where('level_id', '2')->count();
-        return view('user', ['jumlah_data' => $jumlah_data]);
-        
-
+        $user = UserModel::firstOrNew(
+            
+                [
+                    'username' => 'manager33',
+                    'nama' => 'Manager Tiga Tiga',
+                    'password' => Hash::make('12345'),
+                    'level_id' => 2,
+                ]
+            
+        );
+        $user->save();
+        return view('user', ['data' => $user]);
     }
 }
